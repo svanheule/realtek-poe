svanheule/realtek-poe

commit 661ccb9a86294c053618ce1c2d0d85866d3e3740

realtek-poe: Add a ubus call to send frames
As per [1], this commit adds the ability to use `ubus call poe
sendframe` to send a frame to the Broadcom-based PoE controller found
in almost all Realtek-based PoE switches. Frame definitions can be
found here[2].

The `ubus` interface added by this change looks like this:

```txt
root@OpenWrt:/# ubus -v list poe
'poe' @38a3bba9
        "info":{}
        "reload":{}
        "sendframe":{"frame":"String"}
```

One can invoke e.g.:

```txt
ubus call poe sendframe '{"frame": "0x01 0x01"}'
```

to send one frame of type 0x01 with first argument 0x01.

[1]:
https://forum.openwrt.org/t/support-for-rtl838x-based-managed-switches/57875/1432

[2]: https://svanheule.net/switches/software/broadcom_poe_control_protocol

src/main.c
@@ -773,6 +773,47 @@ ubus_poe_info_cb(struct ubus_context *ctx, struct ubus_object *obj,
 	return UBUS_STATUS_OK;
 }
 
+enum {
+	POE_SENDFRAME_STRING,
+	__POE_SENDFRAME_MAX
+};
+
+static const struct blobmsg_policy ubus_poe_sendframe_policy[__POE_SENDFRAME_MAX] = {
+	[POE_SENDFRAME_STRING] = { "frame", BLOBMSG_TYPE_STRING },
+};
+
+static int
+ubus_poe_sendframe_cb(struct ubus_context *ctx, struct ubus_object *obj,
+		   struct ubus_request_data *req, const char *method,
+		   struct blob_attr *msg)
+{
+	struct blob_attr *tb[__POE_SENDFRAME_MAX];
+	blobmsg_parse(ubus_poe_sendframe_policy, __POE_SENDFRAME_MAX, tb, blob_data(msg), blob_len(msg));
+
+	if (!tb[POE_SENDFRAME_STRING])
+		return UBUS_STATUS_INVALID_ARGUMENT;
+
+	char *frame = blobmsg_get_string(tb[POE_SENDFRAME_STRING]);
+
+	unsigned int frame_strlen = strlen(frame);
+	unsigned char cmd[9];
+	unsigned int cmdsize = 0;
+	unsigned char *end = cmd + sizeof(cmd);
+	unsigned int u;
+	unsigned int i = 0;
+
+	while ((cmd + cmdsize < end) && (i < frame_strlen)) {
+		if (sscanf((frame+i), "0x%2x", &u) == 1) {
+		    cmd[cmdsize++] = u;
+			i += 4;
+		} else
+			i += 1;
+	}
+	return poe_cmd_queue(cmd, cmdsize);
+
+	return UBUS_STATUS_OK;
+}
+
 static int
 ubus_poe_reload_cb(struct ubus_context *ctx, struct ubus_object *obj,
 		   struct ubus_request_data *req, const char *method,
@@ -787,6 +828,7 @@ ubus_poe_reload_cb(struct ubus_context *ctx, struct ubus_object *obj,
 static const struct ubus_method ubus_poe_methods[] = {
 	UBUS_METHOD_NOARG("info", ubus_poe_info_cb),
 	UBUS_METHOD_NOARG("reload", ubus_poe_reload_cb),
+	UBUS_METHOD("sendframe", ubus_poe_sendframe_cb, ubus_poe_sendframe_policy),
 };
 
 static struct ubus_object_type ubus_poe_object_type =

commit 5ffe1b1747fd9434196f0d72f723252631a32d5e

realtek-poe: use constant for frame size
Instead of hardcoding the frame size of 12 bytes everywhere in the code,
use a constant to ensure it is used consistently. This fixes an issue
with the "sendframe" command, which would only accept 9 bytes instead,
resulting in the last two bytes of the frame being ignored.

Additionally, as it is frequently used, also define a constant for the
checksum offset.

Fixes: 661ccb9a8629 ("realtek-poe: Add a ubus call to send frames")
Signed-off-by: Sander Vanheule <sander@svanheule.net>

src/main.c
@@ -26,6 +26,9 @@ typedef int (*poe_reply_handler)(struct mcu_state *mcu, uint8_t *reply);
 #define PORT_ID_ALL	0x7f
 #define MAX_RETRIES	5
 
+#define CMD_SIZE	12
+#define OFFSET_CHECKSUM	(CMD_SIZE - 1)
+
 struct mcu {
 	struct uloop_timeout error_timeout;
 	struct list_head pending_cmds;
@@ -36,7 +39,7 @@ struct mcu {
 
 struct cmd {
 	struct list_head list;
-	uint8_t cmd[12];
+	uint8_t cmd[CMD_SIZE];
 	unsigned int num_retries;
 };
 
@@ -209,15 +212,15 @@ static int mcu_queue_cmd(struct mcu *mcu, uint8_t *cmd_buf, size_t len)
 	struct cmd *cmd = malloc(sizeof(*cmd));
 
 	memset(cmd, 0, sizeof(*cmd));
-	memset(cmd->cmd, 0xff, 12);
+	memset(cmd->cmd, 0xff, CMD_SIZE);
 	memcpy(cmd->cmd, cmd_buf, len);
 
 	mcu->cmd_seq++;
 	cmd->cmd[1] = mcu->cmd_seq;
-	cmd->cmd[11] = 0;
+	cmd->cmd[OFFSET_CHECKSUM] = 0;
 
-	for (i = 0; i < 11; i++)
-		cmd->cmd[11] += cmd->cmd[i];
+	for (i = 0; i < OFFSET_CHECKSUM; i++)
+		cmd->cmd[OFFSET_CHECKSUM] += cmd->cmd[i];
 
 	list_add_tail(&cmd->list, &mcu->pending_cmds);
 
@@ -469,7 +472,7 @@ static int poe_reply_4_port_status(struct mcu_state *state, uint8_t *reply)
 		[6] = "Requesting power",
 	};
 
-	for (i = 2; i < 11; i+=2) {
+	for (i = 2; i < OFFSET_CHECKSUM; i+=2) {
 		port = reply[i];
 		pstate = reply[i + 1];
 
@@ -570,10 +573,10 @@ static int mcu_handle_reply(struct mcu *mcu, uint8_t *reply)
 	cmd_id = cmd->cmd[0];
 	cmd_seq = cmd->cmd[1];
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < OFFSET_CHECKSUM; i++)
 		sum += reply[i];
 
-	if (reply[11] != sum) {
+	if (reply[OFFSET_CHECKSUM] != sum) {
 		ULOG_DBG("received reply with bad checksum\n");
 		free(cmd);
 		return -1;
@@ -833,7 +836,7 @@ ubus_poe_sendframe_cb(struct ubus_context *ctx, struct ubus_object *obj,
 	char *frame, *next, *end;
 	size_t cmd_len = 0;
 	unsigned long byte_val;
-	uint8_t cmd[9];
+	uint8_t cmd[CMD_SIZE];
 	int ret;
 
 	if (!poe->hardcore_hacking_mode_en)